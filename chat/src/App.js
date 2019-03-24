a179b76310fad1a5026bbb5bb6b9d50e6a14f7ca
Michael <checomichael2@gmail.com>
2019-03-24 14:17:17 -0400

    cleaned up app and separated firebase logic

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,17 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import firebase from 'firebase';
-import 'firebase/firestore';
-const config = {
-	apiKey: 'AIzaSyDVvkNFJwWIg5u8FtizLRlgHc22lGb_1aI',
-	authDomain: 'chat-app-6079d.firebaseapp.com',
-	databaseURL: 'https://chat-app-6079d.firebaseio.com',
-	projectId: 'chat-app-6079d',
-	storageBucket: 'chat-app-6079d.appspot.com',
-	messagingSenderId: '30890816492',
-};
+import { db } from './firebase';
 
-firebase.initializeApp(config);
-const db = firebase.firestore();
 function App() {
 	const [channels, setChannels] = useState([]);
 	useEffect(() => {
---
+++ chat/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+const config = {
+	apiKey: 'AIzaSyDVvkNFJwWIg5u8FtizLRlgHc22lGb_1aI',
+	authDomain: 'chat-app-6079d.firebaseapp.com',
+	databaseURL: 'https://chat-app-6079d.firebaseio.com',
+	projectId: 'chat-app-6079d',
+	storageBucket: 'chat-app-6079d.appspot.com',
+	messagingSenderId: '30890816492',
+};
+
+firebase.initializeApp(config);
+const db = firebase.firestore();
+
+export { db };
