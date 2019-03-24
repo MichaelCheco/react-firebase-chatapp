bb3f6a4c46c33ac200533dac4aee205e804a236a
Michael <checomichael2@gmail.com>
2019-03-24 14:31:06 -0400

    fixed warnings, and imported only "app" from firebase

--- chat/src/ChannelInfo.js
+++ chat/src/ChannelInfo.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 function ChannelInfo() {
-  return (
-    <div className="ChannelInfo">
-      <div className="Topic">
-        Topic: <input className="TopicInput" value="Awesome stuff" />
-      </div>
-      <div className="ChannelName">#general</div>
-    </div>
-  );
+	return (
+		<div className="ChannelInfo">
+			<div className="Topic">
+				Topic:{' '}
+				<input
+					className="TopicInput"
+					value="Awesome stuff"
+					onChange={() => {}}
+				/>
+			</div>
+			<div className="ChannelName">#general</div>
+		</div>
+	);
 }
 
 export default ChannelInfo;
