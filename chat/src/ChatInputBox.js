53432ec154485edc2c96db48ae7c67ceccc99465
Michael <checomichael2@gmail.com>
2019-03-24 14:44:34 -0400

    saving messages to our db

--- chat/src/ChatInputBox.js
+++ chat/src/ChatInputBox.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { db } from './firebase';
 
 function ChatInputBox() {
-  return (
-    <div className="ChatInputBox">
-      <input className="ChatInput" placeholder="Message #general" />
-    </div>
-  );
+	return (
+		<form
+			onSubmit={event => {
+				event.preventDefault();
+				const value = event.target.elements[0].value;
+				db.collection('channels')
+					.doc('general')
+					.collection('messages')
+					.add({
+						text: value,
+						createdAt: new Date(),
+					});
+			}}
+			className="ChatInputBox">
+			<input className="ChatInput" placeholder="Message #general" />
+		</form>
+	);
 }
 
 export default ChatInputBox;
