11922c7e4b488d0bba54066d8b0639b43eec47c5
Michael <checomichael2@gmail.com>
2019-03-25 10:33:08 -0400

    moved auth to custom hook + cleanup file

--- chat/src/App.js
+++ chat/src/App.js
@@ -3,7 +3,31 @@ import Nav from './Nav';
 import Channel from './Channel';
 import { firebase } from './firebase';
 function App() {
+	const user = useAuth();
+	return user ? (
+		<div className="App">
+			<Nav user={user} />
+			<Channel />
+		</div>
+	) : (
+		<Login />
+	);
+}
+function Login() {
+	const handleSignIn = async () => {
+		const provider = new firebase.auth.GoogleAuthProvider();
+		await firebase.auth().signInWithPopup(provider);
+	};
+	return (
+		<div className="Login">
+			<h1>Chat!</h1>
+			<button onClick={handleSignIn}>Sign in with Google</button>
+		</div>
+	);
+}
+function useAuth() {
 	const [user, setUser] = useState(null);
+
 	useEffect(() => {
 		return firebase.auth().onAuthStateChanged(user => {
 			if (user) {
@@ -17,21 +41,6 @@ function App() {
 			}
 		});
 	}, []);
-	const handleSignIn = async () => {
-		const provider = new firebase.auth.GoogleAuthProvider();
-		await firebase.auth().signInWithPopup(provider);
-	};
-	return user ? (
-		<div className="App">
-			<Nav user={user} />
-			<Channel />
-		</div>
-	) : (
-		<div className="Login">
-			<h1>Chat!</h1>
-			<button onClick={handleSignIn}>Sign in with Google</button>
-		</div>
-	);
+	return user;
 }
-
 export default App;
