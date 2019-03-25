2a52dd946c42055cf04abd2b0c0e819bffec81cc
Michael <checomichael2@gmail.com>
2019-03-25 10:14:43 -0400

    custom hook for pulling data

---
+++ chat/src/useCollection.js
@@ -0,0 +1,26 @@
+import { db } from './firebase';
+import { useEffect, useState } from 'react';
+
+export default function useCollection(path, orderBy) {
+	const [docs, setDocs] = useState([]);
+	useEffect(() => {
+		let collection = db.collection(path);
+		if (orderBy) {
+			collection = collection.orderBy(orderBy);
+		}
+		// .doc('general')
+		// .collection('messages')
+		// .limit(5)
+		return collection.onSnapshot(snapshot => {
+			const docs = [];
+			snapshot.forEach(doc => {
+				docs.push({
+					...doc.data(),
+					id: doc.id,
+				});
+			});
+			setDocs(docs);
+		});
+	}, []);
+	return docs;
+}
