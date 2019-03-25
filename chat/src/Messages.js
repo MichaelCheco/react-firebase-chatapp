c4c0fb3116709f3eb419f69a72b008ff97c03ffd
Michael <checomichael2@gmail.com>
2019-03-25 15:52:19 -0400

    caching users/promises

--- chat/src/Messages.js
+++ chat/src/Messages.js
@@ -56,23 +56,32 @@ function FirstMessageFromUser({ message, showDay }) {
 		</div>
 	);
 }
+const cache = {};
+const pendingCache = {};
+
 function useDoc(path) {
-	const [doc, setDoc] = useState();
+	const [doc, setDoc] = useState(cache[path]);
 	useEffect(() => {
+		if (doc) {
+			return;
+		}
 		let stillMounted = true;
-		db.doc(path)
-			.get()
-			.then(doc => {
-				if (stillMounted) {
-					setDoc({
-						...doc.data(),
-						id: doc.id,
-					});
-				}
-				return () => {
-					stillMounted = false;
+		const pending = pendingCache[path];
+		const promise = pending || (pendingCache[path] = db.doc(path).get());
+
+		promise.then(doc => {
+			if (stillMounted) {
+				const user = {
+					...doc.data(),
+					id: doc.id,
 				};
-			});
+				setDoc(user);
+				cache[path] = user;
+			}
+		});
+		return () => {
+			stillMounted = false;
+		};
 	}, [path]);
 	return doc;
 }
