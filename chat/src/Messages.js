0e76a1f40012c0f32166331c47204900b1a92d42
Michael <checomichael2@gmail.com>
2019-03-25 14:52:42 -0400

    fixed issue with avatar

--- chat/src/Messages.js
+++ chat/src/Messages.js
@@ -39,11 +39,11 @@ function FirstMessageFromUser({ message, showDay }) {
 			<div className="Message with-avatar">
 				<div
 					className="Avatar"
-					style={{ backgroundImage: author ? `url{"${author.photoUrl}}` : '' }}
+					style={{ backgroundImage: author ? `url("${author.photoUrl}")` : '' }}
 				/>
 				<div className="Author">
 					<div>
-						<span className="UserName">{author && author.displayName}</span>
+						<span className="UserName">{author && author.displayName}</span>{' '}
 						<span className="TimeStamp">3:37 PM</span>
 					</div>
 					<div className="MessageContent">{message.text}</div>
